9b1e02ad9eaca7a76aef41946b7222cd267614d2
janeligio <jeligio808@gmail.com>
2019-07-29 19:27:27 -1000

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const reports = require('./routes/api/reports');
 
@@ -20,6 +21,14 @@ mongoose
 
 // Use routes
 app.use('/api/reports', reports);
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
